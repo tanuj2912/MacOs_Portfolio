5b72809035db2c8142186068dda15de43c2f20f0
tanuj2912 <tanujpal415@gmail.com>
2026-01-15 20:56:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// https://youtu.be/j9ZD_hlyHOA?si=kDzB9pJqEyYXtWW_
+
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <main>
+      <Navbar/>
+    </main>
+  )
+}
+export default App;
